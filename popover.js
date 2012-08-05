dd1500cbb3255049bcb391bb27cf558f1b7389e3
Jared Hanson <jaredhanson@gmail.com>
2012-08-04 23:02:38 -0700

    Improve compatibility with Twitter Bootstrap styles.

--- popover.js
+++ popover.js
@@ -2,12 +2,11 @@ define(['tip',
         'anchor/class'],
 function(Tip, clazz) {
   
-  // TODO: Improve compatibility with Twitter Bootstrap styles.
-  
   function Popover(el, options) {
-    Popover.super_.call(this, el, options);
     options = options || {};
-    this.className = options.className || 'popover';
+    options.className = options.className || 'popover'
+    
+    Popover.super_.call(this, el, options);
   }
   clazz.inherits(Popover, Tip);
   
